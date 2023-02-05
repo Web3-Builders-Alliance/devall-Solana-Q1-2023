ce5f9a42cfa8a1f5df889eab696dafc8a958cd02
Alexey Elizarov <felex-ae@ya.ru>
2023-02-05 22:03:35 +0300

    feat(cluster1): escrow with reset

---
+++ Cluster-1/CodeChallenges/solana-escrow/src/error.rs
@@ -0,0 +1,25 @@
+// program specific errors
+
+use solana_program::program_error::ProgramError;
+use thiserror::Error;
+
+#[derive(Error, Debug, Copy, Clone)]
+pub enum EscrowError {
+    /// Invalid instruction
+    #[error("Invalid Instruction")]
+    InvalidInstruction,
+    #[error("Not rent exempt")]
+    NotRentExempt,
+    #[error("Expected amount mismatch")]
+    ExpectedAmountMismatch,
+    #[error("Amount overflow")]
+    AmountOverflow,
+    #[error("Timelock")]
+    Timelock,
+}
+
+impl From<EscrowError> for ProgramError {
+    fn from(e: EscrowError) -> Self {
+        ProgramError::Custom(e as u32)
+    }
+}
